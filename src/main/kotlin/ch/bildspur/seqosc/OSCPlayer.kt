686c5f08c54b08989643a619e2dd43cbf668dacb
Florian Bruggisser <florian@bildspur.ch>
2020-05-30 00:56:37 +0200

    implemented recorder and sender

--- src/main/kotlin/ch/bildspur/seqosc/OSCPlayer.kt
+++ src/main/kotlin/ch/bildspur/seqosc/OSCPlayer.kt
@@ -1,9 +1,19 @@
 package ch.bildspur.seqosc
 
-class OSCPlayer(val address : String, val port : Int, val buffer : OSCBuffer) {
+import com.illposed.osc.OSCSerializerAndParserBuilder
+import com.illposed.osc.transport.udp.OSCPortOut
+import java.net.Inet4Address
+
+
+class OSCPlayer(val host : String, val port : Int, val buffer : OSCBuffer) {
+
+    val client = OSCPortOut(Inet4Address.getByName(host), port)
 
     fun play() {
-
+        buffer.samples.forEach {
+            Thread.sleep(it.delta)
+            client.send(it.packet)
+        }
     }
 
     fun stop() {
--- src/main/kotlin/ch/bildspur/seqosc/OSCSample.kt
+++ src/main/kotlin/ch/bildspur/seqosc/OSCSample.kt
@@ -1,6 +1,5 @@
 package ch.bildspur.seqosc
 
-import com.illposed.osc.OSCMessage
 import com.illposed.osc.OSCPacket
 
 data class OSCSample(var delta : Long, var packet : OSCPacket)
