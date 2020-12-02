4d30d1871dbb5eaa6db949950fc8ef49e22c0c8f
Florian Bruggisser <florian@bildspur.ch>
2020-12-02 15:31:02 +0100

    created playtest

--- seqosclib/src/test/kotlin/ch/bildspur/seqosc/PlayTest.kt
+++ seqosclib/src/test/kotlin/ch/bildspur/seqosc/PlayTest.kt
@@ -5,7 +5,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun main() {
-    val data = Files.readAllBytes(Paths.get("recordings", "test-compressed.osc"))
+    val data = Files.readAllBytes(Paths.get("recordings", "florian.osc"))
 
     val buffer = OSCBuffer()
     buffer.read(ByteBuffer.wrap(data))
@@ -13,10 +13,15 @@ fun main() {
     println("First:")
     println(buffer.samples.first().packet)
 
-    val player = OSCPlayer("localhost", 8000, buffer.speed, buffer)
+    val player = OSCPlayer("localhost", 7800, buffer.speed, buffer)
 
     println("playing ${buffer.samples.size} samples...")
     player.play()
+    player.loop = true
 
+    while(player.playing) {
+        println("Sample: ${player.currentIndex} of ${player.buffer.samples.size}")
+        Thread.sleep(1000 / 1)
+    }
     println("done!")
 }
